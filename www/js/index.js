ced87c7f0c290d1c17614983470996ef652327c4
Korawit <korawit.p@gmail.com>
2018-11-13 02:18:50 +0700

    Google sigin with inapp browser on devices

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,7 @@
-$(function() {
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  console.log('onDeviceReady');
   var config = {
     apiKey: "AIzaSyCF2SSE2-nqgGhef61oSoxB665Tbiq7WJU",
     authDomain: "shopee-16c81.firebaseapp.com",
@@ -8,8 +11,8 @@ $(function() {
     messagingSenderId: "404787069897"
   };
   firebase.initializeApp(config);
-
-  firebase.auth().onAuthStateChanged(function(user) {
+  
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       // User is signed in.
       var displayName = user.displayName;
@@ -27,9 +30,34 @@ $(function() {
       // ...
     }
   });
-
-  $("#google").click(function() {
+  
+  $("#google").click(function () {
+    console.log('Start Google sign in');
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider);
+  
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   });
-});
+}
+
+
+
+
+
